373c4b28935d1ac49ebd5b3154da95833c14269f
priyans619 <priyans.2b@gmail.com>
2023-04-17 16:34:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// var audio = document.createElement('audio');
+// audio.setAttribute('src', 'sound.mp3')
+// audio.loop=true;
+// audio.play();
+
+function play() {
+    var audio = new Audio('sound.mp3');
+    audio.play();
+  }
